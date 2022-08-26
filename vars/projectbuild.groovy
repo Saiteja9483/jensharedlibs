6fc1984d75dbb15e8b8d14736dbf039bd89c5628
Saiteja9483 <106792499+Saiteja9483@users.noreply.github.com>
2022-08-26 16:07:59 +0530

    Update projectbuild.groovy

--- vars/projectbuild.groovy
+++ vars/projectbuild.groovy
@@ -1,5 +1,5 @@
-def call(user,ip) {
-sshagent(['slave']) {
+def call(user,ip.credid) {
+sshagent(['credid']) {
                     sh "mv target/myweb*.war target/app.war"
   sh "scp -o StrictHostKeyChecking=no target/app.war ${user}@${ip}:/opt/tomcat/webapps"
   sh "ssh ${user}@${ip} sudo /opt/tomcat/bin/shutdown.sh"
