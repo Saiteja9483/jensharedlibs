b481e655387db0b832a711b459f76c20b0fefd0b
Saiteja9483 <106792499+Saiteja9483@users.noreply.github.com>
2022-08-26 15:36:53 +0530

    Create project build.groovy

---
+++ project build.groovy
@@ -0,0 +1,9 @@
+def call(){
+  sshagent([${'credId'}]) {
+    // some block 
+    sh "mv target/myweb*.war target/app.war"
+    sh "scp -o StrictHostKeyChecking=no target/app.war ${user}@${ip}:/opt/tomcat/webapps"
+    sh "ssh ${user}@${ip} sudo /opt/tomcat/bin/shutdown.sh"
+    sh "ssh ${user}@${ip} sudo /opt/tomcat/bin/startup.sh"             
+}
+}
