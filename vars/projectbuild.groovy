e4282ad74b00a7c11449f3ebd49931c2fbeef5e3
Saiteja9483 <106792499+Saiteja9483@users.noreply.github.com>
2022-08-26 16:05:47 +0530

    Create projectbuild.groovy

---
+++ vars/projectbuild.groovy
@@ -0,0 +1,9 @@
+def call(user,ip) {
+sshagent(['slave']) {
+                    sh "mv target/myweb*.war target/app.war"
+  sh "scp -o StrictHostKeyChecking=no target/app.war ${user}@${ip}:/opt/tomcat/webapps"
+  sh "ssh ${user}@${ip} sudo /opt/tomcat/bin/shutdown.sh"
+    // some block
+  sh "ssh ${user}@${ip} sudo /opt/tomcat/bin/startup.sh"
+}
+}
