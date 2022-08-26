25d9d2aa390bf3c5f1af7f125ea5e5843c1ada90
Saiteja9483 <106792499+Saiteja9483@users.noreply.github.com>
2022-08-26 15:51:28 +0530

    Update project build.groovy

--- project build.groovy
+++ project build.groovy
@@ -1,5 +1,5 @@
 def call(user,ip,credId){
-  sshagent(['credId']) {
+  sshagent (['credId']) {
     // some block 
     sh "mv target/myweb*.war target/app.war"
     sh "scp -o StrictHostKeyChecking=no target/app.war ${user}@${ip}:/opt/tomcat/webapps"
